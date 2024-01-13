d2a5bc304b3344f69a15b1de4f95e25342281aa1
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-01-13 21:37:07 +0500

    Added some files

---
+++ RabbitMqDocs.N1.Sender/Program.cs
@@ -0,0 +1,36 @@
+﻿
+// create connection factory
+
+using System.Text;
+using RabbitMQ.Client;
+
+var connectionFactory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+// create connection 
+using var connection = await connectionFactory.CreateConnectionAsync();
+
+// create channel
+using var channel = await connection.CreateChannelAsync();
+
+//declare queue
+await channel.QueueDeclareAsync(
+    queue: "hello-world",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+//produce  message
+var message = "Hello Messaging";
+var body = Encoding.UTF8.GetBytes(message);
+
+//publish message
+
+await channel
+    .BasicPublishAsync(
+        exchange: string.Empty,
+        routingKey: "hello-world",
+        body: body);
